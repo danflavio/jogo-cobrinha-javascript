80e1d6f6423bd426e3b4ca86b498289344a5684b
Daniel Flávio <danflavio@gmail.com>
2021-06-24 12:45:48 -0300

    Inserido movimento da cobrinha

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+let direcao = "right";
+let jogo = setInterval(iniciarJogo, 200);
+
+// Posicao da cobrinha
+snake[0] = {
+    x: 7 * box,
+    y: 7 * box
+}
 
 
 function criarBackground() {
@@ -8,4 +17,36 @@ function criarBackground() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBackground();
+function criarCobrinha() {
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function iniciarJogo() {
+    let snakeX;
+    let snakeY;
+    let newHead;
+
+    criarBackground();
+    criarCobrinha();
+
+    snakeX = snake[0].x;
+    snakeY = snake[0].y;
+
+    if (direcao == 'right') snakeX += box;
+    if (direcao == 'left') snakeX -= box;
+    if (direcao == 'up') snakeY -= box;
+    if (direcao == 'down') snakeY += box;
+
+    snake.pop();
+
+    newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    
+    snake.unshift(newHead);
+
+}
